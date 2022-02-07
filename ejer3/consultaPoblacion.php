3604c4f4265c5c0a48f8442035978b742871a39d
Eddy <eddy.gonzalez.chavez.al@iespoligonosur.org>
2022-02-07 12:05:40 +0100

    completoç

--- ejer3/consultaPoblacion.php
+++ ejer3/consultaPoblacion.php
@@ -1,13 +1,34 @@
 <?php //IMPORTANTE: ELIMINA EL ESPACIO ANTES DE LA INTERROGACIÓN
 // Instanciamos un nuevo servidor SOAP
-$uri="https://www.raulprietofernandez.net/images/blog/211/DNI/";
+$uri="http://localhost/DesarrolloEntornoServidor/DWES-UD7/ejer3/";
 $server = new SoapServer(null,array('uri'=>$uri));
-$server->addFunction("letra");
+$server->addFunction("obtenerCiudad");
 $server->handle();
 
 // Función para obtener raíz cuadrada
-function letra($dni) {
-    $resultado=substr("TRWAGMYFPDXBNJZSQVHLCKE",$dni%23,1);
-    return $resultado;
+// function obtenerCiudad($numHabitantes) {
+//     $resultado=substr("TRWAGMYFPDXBNJZSQVHLCKE",$dni%23,1);
+//     return $resultado;
+// }
+
+function getConnection(){
+    $user = 'root';
+    $password = 'root';
+    return new PDO('mysql:host=localhost;dbname=Poblacion', $user, $password);
+}
+
+function obtenerCiudad($numHabitantes){
+    try {
+        $conexion = getConnection();
+        $sql = $conexion->prepare("SELECT * FROM `ciudades` WHERE numHabitantes >= :numHabitantes");
+        $sql->bindParam(":numHabitantes",$numHabitantes);
+        $sql->execute();
+        
+        return $sql->fetchAll();
+        
+    } catch (PDOException $e) {
+        return $e->getMessage();
+    }
+    $conexion=null;
 }
 ?>
